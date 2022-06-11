5285843759eb4046b575881fc9364f5208204b17
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-06-11 17:04:23 +0100

    Licence the things

--- Sharray.hpp
+++ Sharray.hpp
@@ -1,3 +1,12 @@
+/*
+ * Created by Joshua Saxby <joshua.a.saxby@gmail.com>, June 2022
+ * Copyright Joshua Saxby <joshua.a.saxby@gmail.com> 2022
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef COM_SAXBOPHONE_CODLILI_SHARRAY_HPP
 #define COM_SAXBOPHONE_CODLILI_SHARRAY_HPP
 
