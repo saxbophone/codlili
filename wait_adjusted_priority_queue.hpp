de9b9045590d0586bf30120b36a27fd55944aef7
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2022-06-12 16:52:36 +0100

    Create wait_adjusted_priority_queue.hpp

---
+++ wait_adjusted_priority_queue.hpp
@@ -0,0 +1,5 @@
+template <typename T, class Container = std::list<T>>
+class wait_adjusted_priority_queue {
+    // priority is determined by the sum of usage-defined "priority" value and
+    // natural logarithm of the time since it was added
+};
