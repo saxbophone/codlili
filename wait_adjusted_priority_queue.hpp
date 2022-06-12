25e67ec9d01ff9826ae33ed4efdefc8cdbbf162f
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2022-06-12 17:19:19 +0100

    Use exp() rather than log()
    log() grows big at first then tails off, exp does the opposite and that's what we want, i.e.
    - "every 60 minutes raise e to another power" with p = 3600 and q = 1 (assuming we count seconds)

--- wait_adjusted_priority_queue.hpp
+++ wait_adjusted_priority_queue.hpp
@@ -1,7 +1,7 @@
 template <typename T, class Container = std::list<T>>
 class wait_adjusted_priority_queue {
     // priority is determined by the sum of usage-defined "priority" value and
-    // natural logarithm of the time since it was added
-    // we should probably use a tunable log, something like log(n * p) / q,
+    // e raised to the power of the time since it was added
+    // we should probably use a tunable "time out" function, something like exp(n / p) / q,
     // where p and q are tuning parameters
 };
