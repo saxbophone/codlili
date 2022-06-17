497a85b5d47bf064848b28923e0bf0a75bdd1f45
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-06-17 19:11:56 +0100

    Added some sequencecontainer tests

--- tests/SequenceContainer.cpp
+++ tests/SequenceContainer.cpp
@@ -20,8 +20,9 @@ TEMPLATE_PRODUCT_TEST_CASE(
     using X = TestType;
     using T = X::value_type;
     // using A = X::allocator_type;
+    using iterator = X::iterator;
     using const_iterator = X::const_iterator;
-    X&& a = std::move(X({1, 2, 3, 4, 5, 6, 7}));
+    X a = X({1, 2, 3, 4, 5, 6, 7});
     const_iterator p = a.end(); // one past-the-end, valid but not dereferenceable
     const_iterator q = a.begin()++++++; // middle of the container, dereferenceable
     const_iterator q1 = a.begin(), q2 = a.end(); // two const iterators into a such that [q1, q2) is a valid range
@@ -31,4 +32,26 @@ TEMPLATE_PRODUCT_TEST_CASE(
     std::initializer_list<T> il = {3, 6, 9, 12, 14, 16, 18, 23}; // an object of type std::initializer_list<value_type>
     typename X::size_type n = 3; // a value of type X::size_type
     T t = 13; // an lvalue or const rvalue of type X::value_type
+
+    SECTION("X(n, t) Constructs the sequence container holding n copies of t") {
+        CHECK(X(n, t) == X({13, 13, 13}));
+    }
+    SECTION("X(i, j) Constructs the sequence container equal, element-wise, to the range [i,j)") {
+        CHECK(X(i, j) == X({3, 4, 1, 2}));
+    }
+    SECTION("X(il) initializer_list constructor") {
+        CHECK(X(il) == X({3, 6, 9, 12, 14, 16, 18, 23}));
+    }
+    SECTION("a = il Assigns the range represented by il into a") {
+        CHECK(typeid(a = il) == typeid(X&));
+        a = il;
+
+        CHECK(a == X({3, 6, 9, 12, 14, 16, 18, 23}));
+    }
+    SECTION("a.emplace(p, args) Insert an object of type T, constructed with std::forward<Args>(args) before p") {
+        CHECK(typeid(a.emplace(p, 33)) == typeid(iterator));
+    }
+    SECTION("a.insert(p, t) Inserts a copy of t before p") {
+        CHECK(typeid(a.insert(p, t)) == typeid(iterator));
+    }
 }
