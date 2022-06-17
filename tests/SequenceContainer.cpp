3f70ce54ed3213a4177c63d01798bd663c000247
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-06-17 18:24:50 +0100

    Added type definitions for SequenceContainer test case TODO: replace rv with literals, as literals are r-values

--- tests/SequenceContainer.cpp
+++ tests/SequenceContainer.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <deque>
 #include <list>
 #include <typeinfo>
@@ -17,7 +18,18 @@ TEMPLATE_PRODUCT_TEST_CASE(
 ) {
     // alias key definitions to the names used for them in the C++ named requirements
     using X = TestType;
-    // using T = X::value_type;
+    using T = X::value_type;
     // using A = X::allocator_type;
-    X&& a = std::move(X({1, 2, 3}));
+    using const_iterator = X::const_iterator;
+    X&& a = std::move(X({1, 2, 3, 4, 5, 6, 7}));
+    const_iterator p = a.end(); // one past-the-end, valid but not dereferenceable
+    const_iterator q = a.begin()++++++; // middle of the container, dereferenceable
+    const_iterator q1 = a.begin(), q2 = a.end(); // two const iterators into a such that [q1, q2) is a valid range
+    T other[] = {3, 4, 1, 2, 3, 6, 8, 9, 2, 4};
+    auto i = other, j = other + 4; // LegacyInputIterators such that [i, j) is a valid range and that the iterators
+                                   // refer to elements implicitly convertible to value_type
+    std::initializer_list<T> il = {3, 6, 9, 12, 14, 16, 18, 23}; // an object of type std::initializer_list<value_type>
+    typename X::size_type n = 3; // a value of type X::size_type
+    T t = 13; // an lvalue or const rvalue of type X::value_type
+    T&& rv = 24; // a non-const rvalue of type X::value_type
 }
