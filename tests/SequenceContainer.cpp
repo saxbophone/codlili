f4762c7bcc867e328b31c025d2613a0c23573143
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-06-17 22:21:18 +0100

    Use sharray in tests Of course it doesn't work, because everything in it is currently stubbed

--- tests/SequenceContainer.cpp
+++ tests/SequenceContainer.cpp
@@ -7,14 +7,14 @@
 
 #include <catch2/catch_all.hpp>
 
-#include <codlili/list.hpp>
+#include <codlili/sharray.hpp>
 
 
 using namespace com::saxbophone;
 
 TEMPLATE_PRODUCT_TEST_CASE(
     "Type satisfies the SequenceContainer named requirement", "",
-    (std::deque, std::list, std::vector), (char, int, long, float)
+    (std::deque, std::list, std::vector, codlili::sharray), (char, int, long, float)
 ) {
     // alias key definitions to the names used for them in the C++ named requirements
     using X = TestType;
