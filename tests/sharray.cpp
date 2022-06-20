7dcedf0d9ce83deb4495463efb98297a274e1022
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-06-20 06:53:03 +0100

    Wrote unit tests for much more methods

--- tests/sharray.cpp
+++ tests/sharray.cpp
@@ -1,5 +1,7 @@
 #include <cstddef>
 
+#include <vector>
+
 #include <catch2/catch_all.hpp>
 
 #include <codlili/sharray.hpp>
@@ -101,5 +103,85 @@ TEST_CASE("sharray mirrors the API of vector and deque") {
         array.back() = 13;
 
         CHECK(array.back() == 13);
+    }
+    SECTION(".begin() and .end() iterators") {
+        sharray<int> array = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        std::vector<int> other(array.begin(), array.end());
+
+        CHECK(other == std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8}));
+    }
+    SECTION(".cbegin() and .cend() const iterators") {
+        sharray<int> array = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        std::vector<int> other(array.cbegin(), array.cend());
+
+        CHECK(other == std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8}));
+    }
+    SECTION(".rbegin() and .rend() reverse iterators") {
+        sharray<int> array = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        std::vector<int> other(array.rbegin(), array.rend());
+
+        CHECK(other == std::vector<int>({8, 7, 6, 5, 4, 3, 2, 1}));
+    }
+    SECTION(".crbegin() and .crend() const reverse iterators") {
+        sharray<int> array = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        std::vector<int> other(array.crbegin(), array.crend());
+
+        CHECK(other == std::vector<int>({8, 7, 6, 5, 4, 3, 2, 1}));
+    }
+    SECTION(".empty()") {
+        sharray<int> empty;
+        sharray<int> non_empty(42);
+
+        CHECK(empty.empty());
+        CHECK_FALSE(non_empty.empty());
+    }
+    SECTION(".size()") {
+        std::size_t size = (std::size_t)GENERATE(0, 1, 2, 3, 4, 5);
+        sharray<int> array(size);
+
+        CHECK(array.size() == size);
+    }
+    SECTION(".max_size()") {
+        CHECK(typeid(sharray<int>().max_size()) == typeid(sharray<int>::size_type));
+    }
+    SECTION(".reserve()") {
+        std::size_t size = (std::size_t)GENERATE(0, 10, 20, 300, 4000, 50000);
+        sharray<int> array(size);
+        std::size_t to_reserve = (std::size_t)GENERATE(0, 10, 20, 300, 4000, 50000);
+        std::size_t old_capacity = array.capacity();
+
+        array.reserve(to_reserve);
+
+        CHECK(array.size() == size); // size must be unchanged
+        // when to_reserve is less than old_capacity, capacity is unchanged
+        if (to_reserve < old_capacity) {
+            CHECK(array.capacity() == old_capacity);
+        } else { // otherwise, should be greater than or equal to to_reserve
+            CHECK(array.capacity() >= to_reserve);
+        }
+    }
+    SECTION(".capacity()") {
+        std::size_t size = (std::size_t)GENERATE(0, 10, 20, 300, 4000, 50000);
+        sharray<int> array(size);
+
+        CHECK(array.capacity() >= size);
+    }
+    SECTION(".shrink_to_fit()") {
+        WARN("sharray.shrink_to_fit() does nothing. This is standard-compliant.");
+    }
+    SECTION(".clear()") {
+        std::size_t size = (std::size_t)GENERATE(0, 1, 2, 3, 4, 5);
+        sharray<int> array(size);
+        std::size_t old_capacity = array.capacity();
+
+        array.clear();
+
+        CHECK(array.size() == 0);
+        // as per the standard, .clear() must not change capacity (for vector)
+        CHECK(array.capacity() == old_capacity);
     }
 }
